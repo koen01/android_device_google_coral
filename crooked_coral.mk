0cfa3386af43566f9b6cef18a7f0479d6da45d3f
Rashed97 <rashedabdeltawab@gmail.com>
2022-05-26 19:39:17 +0000

    floral: Initial crooked bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ crooked_coral.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common crooked stuff.
+$(call inherit-product, vendor/crooked/config/common.mk)
+$(call inherit-product, vendor/crooked/config/gsm.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/coral/aosp_coral.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+include device/google/coral/device-crooked.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4 XL
+PRODUCT_NAME := crooked_coral
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2960
+TARGET_SCREEN_WIDTH := 1440
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=coral \
+    PRIVATE_BUILD_DESC="coral-user 12 SP2A.220305.012 8177914 release-keys"
+
+BUILD_FINGERPRINT := google/coral/coral:12/SP2A.220305.012/8177914:user/release-keys
---
+++ crooked_flame.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common crooked stuff.
+$(call inherit-product, vendor/crooked/config/common.mk)
+$(call inherit-product, vendor/crooked/config/gsm.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/coral/aosp_flame.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+include device/google/coral/device-crooked.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4
+PRODUCT_NAME := crooked_flame
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2280
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=flame \
+    PRIVATE_BUILD_DESC="flame-user 12 SP2A.220305.012 8177914 release-keys"
+
+BUILD_FINGERPRINT := google/flame/flame:12/SP2A.220305.012/8177914:user/release-keys
