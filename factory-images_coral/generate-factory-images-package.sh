7f0fb83d8a74f1c0deebc402964aca0692dba648
koen01 <koen.erens@gmail.com>
2022-06-25 20:30:51 +0000

    coral: generate-factory-images-package.sh

--- factory-images_coral/generate-factory-images-package.sh
+++ factory-images_coral/generate-factory-images-package.sh
@@ -15,11 +15,11 @@
 # limitations under the License.
 
 source ../../../common/clear-factory-images-variables.sh
-BUILD=5633534
+BUILD=eng.nobody
 DEVICE=coral
-PRODUCT=coral
-VERSION=qd1a.190604.002
-SRCPREFIX=signed-
+PRODUCT=crooked_coral
+VERSION=Q3A.220605.009.B1
+SRCPREFIX=../../../../out/target/product/coral/obj/PACKAGING/target_files_intermediates/
 BOOTLOADER=c2f2-0.2-5619770
 RADIO=g8150-00039-190530-b-5618404
 source ../../../common/generate-factory-images-common.sh
